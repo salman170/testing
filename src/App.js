297caf6f8a8b11f50e3008dcc3029b3bdf26b299
salman170 <salmanansari02120@gmail.com>
2023-05-09 08:54:49 +0530

    Realtime database checking

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { getDatabase, ref, set } from "firebase/database";
+import { app } from './firebase'
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
 
 function App() {
+  //Realtime Data function
+  const putData = () => {
+    set(ref(database, 'users/Imran'), {
+      username: 'Imran',
+      email: "email@gmail.com",
+      id: 2
+
+    });
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Firebase App
       </header>
+      <button onClick={putData}>Add Data</button>
+
     </div>
   );
 }
