35df96f2a186eb37f3ad8731b927118feb0f824c
salman170 <salmanansari02120@gmail.com>
2023-05-11 14:52:53 +0530

    google signup done

--- src/App.js
+++ src/App.js
@@ -8,49 +8,49 @@ const App = () => {
   const Firebase = useFirebase();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
-  useEffect(()=>{
-    onAuthStateChanged(FirebaseAuth, (user)=>{
-      if(user) setUser(user)
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, (user) => {
+      if (user) setUser(user);
       else {
-        console.log('You are logged out')
-        setUser(null)
+        console.log('You are logged out');
+        setUser(null);
       }
-    })
-  }, [user])
+    });
+  }, [user]);
 
-  if(user===null){
-    return <div>
-    <h1>Firebase</h1>
-    <input
-      onChange={(e) => setEmail(e.target.value)}
-      value={email}
-      type='email'
-      placeholder='Enter Email'
-    />
-    <input
-      onChange={(e) => setPassword(e.target.value)}
-      value={password}
-      type='password'
-      placeholder='Enter password'
-    />
-    <button
-      onClick={() => {
-        Firebase.signupUserWithEmailAndPassword(email, password);
-        // Firebase.putData('users/' + 'saif', { email, password });
-      }}
-    >
-      SignUp
-    </button>
-    <h1>Signin</h1>
-    <SignupPage/>
-  </div>
+  if (user === null) {
+    return (
+      <div>
+        <h1>Firebase</h1>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          type='email'
+          placeholder='Enter Email'
+        />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          type='password'
+          placeholder='Enter password'
+        />
+        <button
+          onClick={() => {
+            Firebase.signupUserWithEmailAndPassword(email, password);
+            // Firebase.putData('users/' + 'saif', { email, password });
+          }}
+        >
+          SignUp
+        </button>
+        <h1>Signin</h1>
+        <SignupPage />
+      </div>
+    );
   }
 
-  return (
-   <div>Hello world</div>
-  );
+  return <div>Hello world</div>;
 };
 
 export default App;
